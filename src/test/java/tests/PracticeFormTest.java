a4d091d1aed68da3763d3df812859a5c9eb75c96
andrejlutovinov <andreilute@gmail.com>
2025-01-13 13:05:53 +0300

    added fdhshdf

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -17,6 +17,7 @@ public class PracticeFormTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
 
     }
 
@@ -43,8 +44,8 @@ public class PracticeFormTest {
         $(".react-datepicker__month-select").click();
         $(".react-datepicker__year-select").selectOption("1994");
         $(".react-datepicker__month-select").selectOption("January");
-
         $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text("30")).click();
+
         $("#userForm #subjectsInput").setValue("History");
         $("#userForm .subjects-auto-complete__menu").click();
 
